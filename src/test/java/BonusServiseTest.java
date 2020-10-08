b1cfc53c6ba634306d529c2dad5d0356c3b67890
Alena Korovina <evtam-29@mail.ru>
2020-10-08 21:12:49 +0300

    change f

--- src/test/java/BonusServiseTest.java
+++ src/test/java/BonusServiseTest.java
@@ -1,8 +1,75 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class BonusServiseTest {
+@org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredUnderLimit(){
+            BonusService service=new BonusService();
 
-    @org.junit.jupiter.api.Test
-    void calculate() {
-    }
-}
+            // подготавливаем данные:
+            long amount=1000_60;
+            boolean registered=false;
+            long expected=30;
+
+            // вызываем целевой метод:
+            long actual=service.calculate(amount,registered);
+            assertEquals(expected,actual);
+
+            // производим проверку (сравниваем ожидаемый и фактический):
+            // если true - то PASS
+            // если false - то FAIL
+
+            //boolean passed = expected == actual;
+
+            // выводим результат
+            // System.out.println(passed);
+            }
+
+@org.junit.jupiter.api.Test
+    void shouldCalculateForNotRegisteredOverLimit(){
+            BonusService service=new BonusService();
+
+
+            long amount=100_000_60;
+            boolean registered=false;
+            long expected=500;
+
+
+            long actual=service.calculate(amount,registered);
+            assertEquals(expected,actual);
+            }
+
+@org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredOverLimit(){
+            BonusService service=new BonusService();
+
+
+            long amount=100_000_60;
+            boolean registered=true;
+            long expected=3000;
+
+
+            long actual=service.calculate(amount,registered);
+            assertEquals(expected,actual);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
